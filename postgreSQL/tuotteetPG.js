4258d35344de72787c622ec1d4c9788a4d4204a9
Aleksi1K <59920287+Aleksi1K@users.noreply.github.com>
2020-03-22 10:24:15 +0200

    tuotteista tuotenro korjattu

--- postgreSQL/tuotteetPG.js
+++ postgreSQL/tuotteetPG.js
@@ -14,38 +14,38 @@ const conopts = {
 const pool=new Pool(conopts)  
 
 const haetuotteet = (t) => {
-    pool.query('SELECT * from tuotteet ORDER BY id', (err, results) => {
+    pool.query('SELECT * from tuotteet ORDER BY tuotenro', (err, results) => {
         if (err) throw err;
         console.dir(results);
         t(results.rows);
     })
 }
-const haetuote = (id, t) => {
-    pool.query('SELECT * FROM tuotteet WHERE id=$1', [id], (err, results) => {
+const haetuote = (tuotenro, t) => {
+    pool.query('SELECT * FROM tuotteet WHERE tuotenro=$1', [tuotenro], (err, results) => {
         if (err) throw err;
         console.dir(results.rows);
         t(results.rows);
     })
 }
 const luotuote = (uusituote, t) => {
-    const { tuotenimi, tuntitavoite } = uusituote;
-    pool.query('INSERT INTO tuotteet (tuotenimi, tuntitavoite) VALUES ($1, $2)', [tuotenimi, tuntitavoite], (err, results) => {
+    const { tuotenro, tuotenimi, tuntitavoite } = uusituote;
+    pool.query('INSERT INTO tuotteet (tuotenro, tuotenimi, tuntitavoite) VALUES ($1, $2, $3)', [tuotenro, tuotenimi, tuntitavoite], (err, results) => {
         if (err) throw err;
         console.dir(results);
         t(results.rowCount);
     })
 }
 
-const paivitatuote = (tuote, id, t) => {
+const paivitatuote = (tuote, tuotenro, t) => {
     const { tuotenimi, tuntitavoite } = tuote;
-    pool.query('UPDATE tuotteet SET tuotenimi=$1, tuntitavoite=$2 WHERE id=$3', [tuotenimi, tuntitavoite, id], (err, results) => {
+    pool.query('UPDATE tuotteet SET tuotenimi=$1, tuntitavoite=$2 WHERE tuotenro=$3', [tuotenimi, tuntitavoite, tuotenro], (err, results) => {
         if (err) throw err;
         console.dir(results);
         t(results.rowCount);
     })
 }
-const poistatuote = (id, t) => {
-    pool.query('DELETE FROM tuotteet WHERE id=$1', [id], (err, results) => {
+const poistatuote = (tuotenro, t) => {
+    pool.query('DELETE FROM tuotteet WHERE tuotenro=$1', [tuotenro], (err, results) => {
         if (err) throw err;
         console.dir(results);
         t(results.rowCount);
